a1936e33d85656b12a31d2d12f9ca375871b9475
sdivyarth <55125599+sdivyarth@users.noreply.github.com>
2020-04-10 02:54:25 +0530

    Update CNN.py
    correct scaling :/

--- CNN.py
+++ CNN.py
@@ -48,22 +48,23 @@ def split_sequence(sequence, n_steps):
 		y.append(seq_y)
 	return np.array(X), np.array(y)
 
+sequence= data["India"]
 n_steps=4
 n_features=1
 n=int(len(data)*0.8)
+scalerY= MinMaxScaler()
+scalerY.fit_transform(sequence[:n].values.reshape((n,1)))
+sequence = scalerY.transform(sequence.values.reshape((len(sequence),1)))
 X,y=split_sequence(sequence,n_steps)
 X_=X[:n,:]
 y_=y[:n]
 
-scaler = MinMaxScaler()
-scaler.fit_transform(X_)
-X_train=scaler.transform(X_).reshape((n,n_steps,1))
-X_test=scaler.transform(X[n:,:]).reshape((len(X)-n,n_steps,1))
 
-scalerY= MinMaxScaler()
-scalerY.fit_transform(y_.reshape((len(y_),1)))
-y_train=scalerY.transform(y_.reshape((len(y_),1))).reshape(y_.shape)
-y_test=scalerY.transform(y[n:].reshape((len(y[n:]),1))).reshape(y[n:].shape)
+X_train=X[:n].reshape((n,n_steps,1))
+X_test=X[n:].reshape((len(X)-n,n_steps,1))
+
+y_train=y[:n].reshape((n,1))
+y_test=y[n:].reshape((len(X)-n,1))
 
 from keras.models import Sequential
 from keras.layers import Conv1D,MaxPooling1D,Conv2D,Activation,Dense,MaxPooling2D,Flatten,BatchNormalization,Dropout,GlobalAveragePooling2D
@@ -77,93 +78,38 @@ model.add(Dense(50, activation='relu'))
 model.add(Dense(1))
 model.compile(optimizer='adam', loss='mse')
 # fit model
-
-history = model.fit(X_train, y_train, validation_split=0.10, epochs = 150 ,batch_size=1)
-#model.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
-#history = model.fit(X_train, y_train, validation_split=0.10, epochs = 150 ,batch_size=1)
-
-model = Sequential()
-model.add(Conv1D(filters=32, kernel_size=2, activation='relu',padding='causal',dilation_rate=1, input_shape=(n_steps, n_features)))
-model.add(Conv1D(filters=32, kernel_size=2, activation='relu',padding='causal',dilation_rate=2,))
-model.add(Conv1D(filters=32, kernel_size=2, activation='relu',padding='causal',dilation_rate=4,))
-model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.2))
-model.add(Dense(1))
-
-
-
-
 from keras.callbacks import EarlyStopping
 es=EarlyStopping(monitor='val_loss', min_delta=0, patience=20, verbose=0, mode='auto', baseline=None, restore_best_weights=True)
+history = model.fit(X_train, y_train, validation_split=0.10, epochs = 150 ,batch_size=1)
+
+
 
 plt.plot(history.history['loss'],label='loss')
 plt.plot(history.history['val_loss'],label='val_loss')
 plt.legend()
 plt.show()
 
-yH=scalerY.inverse_transform(model.predict(scaler.transform(X).reshape((len(X),n_steps,n_features))))
+yH=scalerY.inverse_transform(model.predict(X.reshape((len(X),n_steps,n_features))).reshape((len(X),1)))
 
 plt.plot(yH,label='pred')
-plt.plot(y,label='actual')
+plt.plot(scalerY.inverse_transform(y.reshape((len(y),1))),label='actual')
 plt.legend()
 
-'''
-
-
-from keras.models import Model
-from keras.layers import Input, Conv1D, Dense, Dropout, Lambda, concatenate
-
-def get_model(input_shape=None,n_filters=32,filter_width=2,dilation_rates=[2**i for i in range(8)]):
-    
-    img_input=Input(input_shape)
-    x=img_input
-    for dilation_rate in dilation_rates:
-        x = Conv1D(filters=n_filters,
-                   kernel_size=filter_width, 
-                   padding='causal',
-                   dilation_rate=dilation_rate)(x)
-    x = Dense(128, activation='relu')(x)
-    x = Dropout(.5)(x)
-    x = Dense(1)(x)
-
-    return Model(img_input,x)
-
-input_shape=X[0].shape
-n_filters = 32 
-filter_width = 2
-dilation_rates = [2**i for i in range(8)] 
-
-model=get_model(input_shape=input_shape)
-
-from keras.callbacks import EarlyStopping
-es=EarlyStopping(monitor='val_loss', min_delta=0, patience=20, verbose=0, mode='auto', baseline=None, restore_best_weights=True)
-model.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
-
-history = model.fit(X_train, y_train, validation_split=0.10, epochs = 150 ,batch_size=3,callbacks=[es])
-
-plt.plot(history.history['loss'],label='loss')
-plt.plot(history.history['val_loss'],label='val_loss')
+#this functidaton takes an x as an input; in *transformed* form gives the next x in transformed form
+x=X_test[-1]
+def make_data(x,model,iter0,scalerY):
+    x_=x.reshape(1,x.shape[0],1)
+    z=model.predict(x_)
+    ret=np.concatenate((x_[:,1:,:],z.reshape((1,1,1))),axis=1)
+    y=[ret[:,-1,:]]
+    for i in range(iter0):
+        z=model.predict(ret)
+        ret=np.concatenate((ret[:,1:,:],z.reshape((1,1,1))),axis=1)
+        y.append(ret[:,-1,:])
+    return scalerY.inverse_transform(np.array(y).reshape((len(y),1)))
+yF=make_data(x,model,15,scalerY)
+y1=data["India"].values.reshape(len(data),1)
+y_fin=np.concatenate((y1,yF),axis=0)
+plt.plot(y_fin,label='final')
+plt.plot(y1,label='original')
 plt.legend()
-plt.show()
-
-history_seq = Input(shape=X[0].shape)
-x = history_seq
-
-for dilation_rate in dilation_rates:
-    x = Conv1D(filters=n_filters,
-               kernel_size=filter_width, 
-               padding='causal',
-               dilation_rate=dilation_rate)(x)
-
-x = Dense(128, activation='relu')(x)
-x = Dropout(.2)(x)
-x = Dense(1)(x)
-
-def slice(x, seq_length):
-    return x[:,-seq_length:,:]
-
-pred_seq_train = Lambda(slice, arguments={'seq_length':3})(x)
-
-model = Model(history_seq, pred_seq_train)
-
-'''
