8bce15702ce8a3b968ecbee28ac890d7c44a179f
sdivyarth <55125599+sdivyarth@users.noreply.github.com>
2020-04-30 20:28:02 +0530

    Update CNN.py

--- CNN.py
+++ CNN.py
@@ -17,10 +17,13 @@ import math
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_squared_error
+from sklearn.metrics import r2_score
 
 # Keep same random seed to get reproducible results || When ran on GPU results not reproducible
-np.random.seed(7)
-data = pd.read_csv('COVID19_5apr.csv')
+
+from tensorflow import set_random_seed
+set_random_seed(1)
+data = pd.read_csv('COVID13_04.csv')
 
 data['Country'] = data['Country/Region']
 ser=pd.Series(data=data[data['Province/State'].notnull()]['Country'].values+" "+data[data['Province/State'].notnull()]['Province/State'].values,index=data[data['Province/State'].notnull()]['Country'].index)
@@ -29,10 +32,12 @@ index= data[data['Province/State'].notnull()]['Country'].index
 for i in index:
   data.at[i,"Country"]=ser[i]
 data=data.set_index("Country")
-data=data.drop(['Province/State', 'Country/Region', 'Lat', 'Long'],axis=1).T
+data=data.drop(['Province/State', 'Country/Region', 'Lat', 'Long','Recovered','Deaths','Date'],axis=1).T
 
-dataset=data["India"].value
-dataset=dataset.reshape((len(dataset),1))
+dataset=data["India"].values.reshape((-1,1))
+dataset=dataset.reshape((len(dataset),1))[-30:]
+data= pd.read_csv('https://api.covid19india.org/csv/latest/state_wise_daily.csv')
+dataset=data[data.Status=='Confirmed']['TT'].cumsum().values
 
 def split_sequence(sequence, n_steps):
 	X, y = list(), list()
@@ -48,55 +53,74 @@ def split_sequence(sequence, n_steps):
 		y.append(seq_y)
 	return np.array(X), np.array(y)
 
-sequence= data["India"]
-n_steps=4
-n_features=1
-n=int(len(data)*0.8)
-scalerY= MinMaxScaler()
-scalerY.fit_transform(sequence[:n].values.reshape((n,1)))
-sequence = scalerY.transform(sequence.values.reshape((len(sequence),1)))
-X,y=split_sequence(sequence,n_steps)
-X_=X[:n,:]
-y_=y[:n]
+lst=[42,41,39,38]
+yH=np.zeros(y.shape)
+for iter0 in range(0,5):
+    np.random.seed(i)
+#    dataset=data["India"].values.reshape((-1,1))
+    sequence= dataset
+    n_steps=4
+    n=lst[n_steps-1]
+    n_features=1    
+    scalerY= MinMaxScaler()
+    scalerY.fit_transform(sequence[:n].reshape((n,1)))
+    sequence = scalerY.transform(sequence.reshape((len(sequence),1)))
+    X,y=split_sequence(sequence,n_steps)
+    X_=X[:n,:]
+    y_=y[:n]
+    
+    X_train=X[:n].reshape((n,n_steps,1))
+    X_test=X[n:].reshape((len(X)-n,n_steps,1))
+    
+    y_train=y[:n].reshape((n,1))
+    y_test=y[n:].reshape((len(X)-n,1))
+    
+    from tensorflow.keras.models import Sequential
+    from tensorflow.keras.layers import Conv1D,MaxPooling1D,Conv2D,Activation,Dense,MaxPooling2D,Flatten,BatchNormalization,Dropout,GlobalAveragePooling2D
+    from tensorflow.keras.optimizers import Adam
+    
+    model = Sequential()
+    model.add(Conv1D(filters=32, strides=1,kernel_size=5,padding='same', activation='relu', input_shape=(n_steps, n_features)))
+    model.add(Conv1D(filters=32, strides=1,kernel_size=2,padding='same', activation='relu'))
+    model.add(Conv1D(filters=64, strides=1,kernel_size=2,padding='same', activation='relu'))
+    model.add(Conv1D(filters=64, strides=1,kernel_size=2,padding='same', activation='relu'))
+    model.add(Flatten())
+    model.add(Dense(1))
+    model.compile(optimizer='adam', loss='mse')
+    # fit model
+    from keras.callbacks import EarlyStopping
+    es=EarlyStopping(monitor='val_loss', min_delta=0, patience=20, verbose=0, mode='auto', baseline=None, restore_best_weights=True)
+    history = model.fit(X_train, y_train, validation_split=0.10, epochs = 200,batch_size=1)
+    x=X_test[-1]
+    if iter0==0:
+        yF=make_data(x,model,20,scalerY)/5
+        yH=scalerY.inverse_transform(model.predict(X.reshape((len(X),n_steps,n_features))).reshape((len(X),1)))/5
+    else:
+        yF+=make_data(x,model,20,scalerY)/5
+        yH+=scalerY.inverse_transform(model.predict(X.reshape((len(X),n_steps,n_features))).reshape((len(X),1)))/5
+
+from sklearn.metrics import r2_score
+ya=scalerY.inverse_transform(y.reshape((len(y),1)))
+r2_train=r2_score(ya[:n],yH[:n])
+print(r2_train)
+r2_test=r2_score(ya[n:],yH[n:])
+print(r2_test)
+fig, ax = plt.subplots( nrows=1, ncols=1)
+plt.plot(yH,label='pred')
+plt.plot(ya,label='actual')
+plt.legend()
+fig.savefig("pred_"+str(n_steps)+"_"+str(r2_test)+"_"+str(r2_train)+".png")
 
 
-X_train=X[:n].reshape((n,n_steps,1))
-X_test=X[n:].reshape((len(X)-n,n_steps,1))
 
-y_train=y[:n].reshape((n,1))
-y_test=y[n:].reshape((len(X)-n,1))
-
-from keras.models import Sequential
-from keras.layers import Conv1D,MaxPooling1D,Conv2D,Activation,Dense,MaxPooling2D,Flatten,BatchNormalization,Dropout,GlobalAveragePooling2D
-from keras.optimizers import Adam
-
-model = Sequential()
-model.add(Conv1D(filters=64, kernel_size=2, activation='relu', input_shape=(n_steps, n_features)))
-model.add(MaxPooling1D(pool_size=2))
-model.add(Flatten())
-model.add(Dense(50, activation='relu'))
-model.add(Dense(1))
-model.compile(optimizer='adam', loss='mse')
-# fit model
-from keras.callbacks import EarlyStopping
-es=EarlyStopping(monitor='val_loss', min_delta=0, patience=20, verbose=0, mode='auto', baseline=None, restore_best_weights=True)
-history = model.fit(X_train, y_train, validation_split=0.10, epochs = 150 ,batch_size=1)
-
-
-
+fig, ax = plt.subplots( nrows=1, ncols=1 )
 plt.plot(history.history['loss'],label='loss')
 plt.plot(history.history['val_loss'],label='val_loss')
 plt.legend()
 plt.show()
 
-yH=scalerY.inverse_transform(model.predict(X.reshape((len(X),n_steps,n_features))).reshape((len(X),1)))
-
-plt.plot(yH,label='pred')
-plt.plot(scalerY.inverse_transform(y.reshape((len(y),1))),label='actual')
-plt.legend()
-
 #this functidaton takes an x as an input; in *transformed* form gives the next x in transformed form
-x=X_test[-1]
+
 def make_data(x,model,iter0,scalerY):
     x_=x.reshape(1,x.shape[0],1)
     z=model.predict(x_)
@@ -107,9 +131,13 @@ def make_data(x,model,iter0,scalerY):
         ret=np.concatenate((ret[:,1:,:],z.reshape((1,1,1))),axis=1)
         y.append(ret[:,-1,:])
     return scalerY.inverse_transform(np.array(y).reshape((len(y),1)))
-yF=make_data(x,model,15,scalerY)
-y1=data["India"].values.reshape(len(data),1)
-y_fin=np.concatenate((y1,yF),axis=0)
+
+x=X_test[-1]
+#yF=make_data(x,model,20,scalerY)
+
+y1=scalerY.inverse_transform(np.concatenate((y_train,y_test),axis=0))
+y_fin=np.concatenate((yH,yF),axis=0)
 plt.plot(y_fin,label='final')
 plt.plot(y1,label='original')
+plt.savefig("next_"+str(n_steps)+".png")
 plt.legend()
